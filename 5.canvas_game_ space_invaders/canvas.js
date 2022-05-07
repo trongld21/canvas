a658cd68239373177b6691a80d32e7695643baab
ducctrong <trongld21@gmail.com>
2022-05-08 02:05:27 +0700

    5.Game space invaders - Some problem - Step 3: Create projectiles

--- 5.canvas_game_ space_invaders/canvas.js
+++ 5.canvas_game_ space_invaders/canvas.js
@@ -46,7 +46,38 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(position, velocity) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 3;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'red';
+        c.fill();
+        c.closePath();
+    }
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+    }
+}
+
 const player = new Player();
+const projectiles = [new Projectile({
+    position: {
+        x: 300,
+        y: 300
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})];
 const keys = {
     a: {
         pressed: false
@@ -66,7 +97,9 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.draw();
     player.update();
-
+    projectiles.forEach(projectile => {
+        projectile.update();
+    })
     if (keys.a.pressed && player.position.x >= 0) {
         player.velocity.x = -7;
         player.rotation = -0.15;
@@ -93,6 +126,16 @@ addEventListener('keydown', ({ key }) => {
             break;
         case ' ':
             console.log('space ' + key);
+            // projectiles.push(new Projectile({
+            //     position: {
+            //         x: player.position.x,
+            //         y: player.position.y
+            //     },
+            //     velocity: {
+            //         x: 0,
+            //         y: -5
+            //     }
+            // }))
             keys.space.pressed = true;
             break;
     }
